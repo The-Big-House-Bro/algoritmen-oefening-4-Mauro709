961ab17248433ea9213e4133e94a71ac15490821
Mauro709 <mauroraman@outlook.com>
2025-03-22 11:31:40 +0100

    klaar

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,13 @@
-def countTargetPairs():
+def countTargetPairs(nums):
+    pairs = 0
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] < target:
+                pairs = pairs + 1
+    return pairs
+
+
+nums = [-6,2,5,-2,-7,-1,3] 
+target = -2
+resultaat = countTargetPairs(nums)
+print(resultaat)
